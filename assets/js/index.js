ef71fc797267da9a6a8c71121053c9608e5ae3ed
Arvid Edenheim <arved490@student.liu.se>
2019-08-24 00:04:07 +0200

    Changed to english

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,24 @@
+window.onresize = function(){
+    document.body.height = window.innerHeight
+    const inners = document.getElementsByClassName('inner')
+    for (var i = 0; i < inners.length; i++) {
+        inners[i].style.height = i === inners.length - 1 ? `${window.innerHeight - 60}px` : `${window.innerHeight}px`
+    }
+}
+
 window.onload = () => {
+    window.onresize()
 
     /* Footer with current year */
     const footer = document.getElementById("footer")
     const year = new Date().getFullYear()
-    footer.innerHTML = `<a href="mailto:arvid@edenheim.se">arvid@edenheim.se</a>| 073-5320103 | Registrerad för F-Skatt | ${year}`
-
-
-
-
+    footer.innerHTML = `<a href="mailto:arvid@edenheim.se">arvid@edenheim.se</a>| Registrerad för F-Skatt | ${year}`
 
 
 
 
     /* Section scrolling  */
-    const sectionHeight = document.getElementsByClassName('inner')[1].offsetTop
+    // const sectionHeight = document.getElementsByClassName('inner')[1].offsetTop
     let currentSection = 0
     let shouldScroll = true
     let lastEvent;
@@ -62,7 +67,7 @@ window.onload = () => {
     }
 
     function handleScroll(event) {
-        event.preventDefault()
+        // event.preventDefault()
         if (shouldScroll && Math.abs(event.deltaY) > lastScroll) {
             const isDown = event.deltaY > 0
             event.deltaY = 0
@@ -77,7 +82,7 @@ window.onload = () => {
         currentSection = (currentSection < 2 && isDown) ? (currentSection + 1) : ((currentSection > 0 && !isDown) ? currentSection - 1 : currentSection)
         document.getElementsByClassName('inner')[currentSection].classList.add('active')
         document.getElementsByClassName('top-navigator')[0].className = currentSection > 0 ? 'top-navigator active' : 'top-navigator'
-        document.getElementById('scroll-content').style.transform = `translateY(-${101 * currentSection}vh)`
+        document.getElementById('scroll-content').style.transform = `translateY(-${(window.innerHeight+10) * currentSection}px)`
         setTimeout(handleHasScrolled, 800)
     }
 
@@ -119,6 +124,28 @@ window.onload = () => {
         }
         isPlaying = !isPlaying
     }
+
+
+
+    /* Topics */
+    let topic = 0
+    const topics = ['Data Science', 'Web Development', 'Programming', 'Mathematics', 'Technology']
+    const topicElement = document.getElementById('hero-topics')
+    const transitionTime = 500
+    const timeout = 4000
+
+    const setTopic = () => {
+        topicElement.innerHTML = topics[topic++ % topics.length]
+        topicElement.classList.toggle('visible')
+        setTimeout(() => {
+            topicElement.classList.toggle('visible')
+            setTimeout(setTopic, transitionTime)
+        }
+        ,timeout)
+    }
+
+    setTopic()
+
 }
 
 

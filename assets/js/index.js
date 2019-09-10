2b4716ad24619ae2a0730e3be0b9feee99be3706
Arvid Edenheim <arved490@student.liu.se>
2019-09-10 23:01:56 +0200

    Updates buzzwords

--- assets/js/index.js
+++ assets/js/index.js
@@ -129,7 +129,7 @@ window.onload = () => {
 
     /* Topics */
     let topic = 0
-    const topics = ['Data Science', 'Web Development', 'Programming', 'Economics', 'Technology']
+    const topics = ['Data Science', 'Web Development', 'Tinkering', 'Problem Solving']
     const topicElement = document.getElementById('hero-topics')
     const transitionTime = 500
     const timeout = 4000

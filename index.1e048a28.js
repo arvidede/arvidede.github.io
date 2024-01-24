3b76c3102902536f5aac12d56662811662fa6eea
Arvid Edenheim <arvidede@icloud.com>
2024-01-24 23:24:52 +0100

    Updates

---
+++ index.1e048a28.js
@@ -0,0 +1,2 @@
+window.onload=()=>{let e=!1;toggleAudio=()=>{const o=document.getElementById("audio");e?o.pause():o.play();const l=document.getElementById("toggle");e?(l.classList.remove("fa-volume-high"),l.classList.add("fa-volume-off")):(l.classList.remove("fa-volume-off"),l.classList.add("fa-volume-high")),e=!e}};
+//# sourceMappingURL=index.1e048a28.js.map
